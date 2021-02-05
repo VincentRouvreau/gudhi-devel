da44fec4d0d4f00575ca980e83de43f59a4cf2a1
ROUVREAU Vincent <vincent.rouvreau@inria.fr>
2021-02-05 09:18:29 +0100

    Use cell range in examples

--- src/Hasse_diagram/example/Hasse_diagram_basic_example.cpp
+++ src/Hasse_diagram/example/Hasse_diagram_basic_example.cpp
@@ -18,13 +18,15 @@ int main(int argc, char** argv)
   edge2->get_boundary().emplace_back(vertex1, -1);
   edge2->get_boundary().emplace_back(vertex2, -1);
 
-  // we can input the cells in any order into the vector of cells:
-  std::vector< Cell* > cells_vector = {vertex1, vertex2, edge1, edge2};
-
   using Hasse_diagram = Gudhi::hasse_diagram::Hasse_diagram<Cell>;
-  Hasse_diagram hasse_diag(cells_vector);
+  using Cell_range = Hasse_diagram::Cell_range;
+
+  // we can input the cells in any order into the vector of cells:
+  Cell_range cells = {vertex1, vertex2, edge1, edge2};
+
+  Hasse_diagram hasse_diag(cells);
   std::cout << hasse_diag.full_signature_of_the_structure() << std::endl;
 
-  for (auto cell : cells_vector)
+  for (auto cell : cells)
     delete cell;
 }
