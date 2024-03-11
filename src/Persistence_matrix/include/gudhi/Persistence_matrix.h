d05383de108ddd9e9c67cd60feb96557e7b3824e
Vincent Rouvreau <vincent.rouvreau@inria.fr>
2024-03-11 18:55:44 +0100

    Persistence_cohomology-like interface for Persistence_matrix

---
+++ src/Persistence_matrix/include/gudhi/Persistence_matrix.h
@@ -0,0 +1,104 @@
+/*    This file is part of the Gudhi Library - https://gudhi.inria.fr/ - which is released under MIT.
+ *    See file LICENSE or go to https://gudhi.inria.fr/licensing/ for full license details.
+ *    Author(s):       Vincent Rouvreau
+ *
+ *    Copyright (C) 2024 Inria
+ *
+ *    Modification(s):
+ *      - YYYY/MM Author: Description of the modification
+ */
+
+#ifndef PERSISTENCE_MATRIX_H
+#define PERSISTENCE_MATRIX_H
+
+#include <vector>
+#include <iostream>
+#include <algorithm>  // for std::sort
+#include <iterator>  // for std::advance
+
+#include <gudhi/matrix.h>
+#include <gudhi/persistence_matrix_options.h>
+
+namespace Gudhi {
+namespace persistence_matrix {
+
+template <class FilteredComplex, class Options = Gudhi::persistence_matrix::Default_options<> >
+class Persistence_matrix {
+ public:
+  explicit Persistence_matrix(FilteredComplex& cpx)
+    : cpx_(&cpx), matrix_() {}
+
+  void init_coefficients(int charac) {
+    charac_ = charac;
+  }
+
+  using Filtration_value = typename FilteredComplex::Filtration_value;
+  void compute_persistent_cohomology([[maybe_unused]] Filtration_value min_interval_length = 0) {
+    auto filtration_begin = cpx_->filtration_simplex_range().begin();
+    auto filtration_end = cpx_->filtration_simplex_range().end();
+    for (auto f_simplex : cpx_->filtration_simplex_range()) {
+      std::vector<int> boundary_indexes = {};
+      std::clog << "   { ";
+      for (auto b_simplex : cpx_->boundary_simplex_range(f_simplex)) {
+        auto it = std::find(filtration_begin, filtration_end, b_simplex);
+        // A boundary must be found in filtrations list
+        assert(it != filtration_end);
+        boundary_indexes.push_back(std::distance(filtration_begin, it));
+        std::clog << std::distance(filtration_begin, it) << ", ";
+      }
+      std::clog << "}\n";
+      std::sort(boundary_indexes.begin(), boundary_indexes.end());
+      matrix_.insert_boundary(boundary_indexes);
+    }
+  }
+
+  using Barcode = typename Matrix<Options>::Bar;
+
+  struct cmp_intervals_by_length {
+    explicit cmp_intervals_by_length(FilteredComplex * sc)
+        : sc_(sc),
+          filtration_size_(std::distance(sc_->filtration_simplex_range().begin(),
+                                         sc_->filtration_simplex_range().end())) {
+    }
+    bool operator()(const Barcode & bc1, const Barcode & bc2) {
+      //if (bc1.dim != bc2.dim) return bc1.dim > bc2.dim;
+      if (bc1.death > filtration_size_) return true;
+      if (bc2.death > filtration_size_) return false;
+      auto filtration_begin = sc_->filtration_simplex_range().begin();
+      return (sc_->filtration(*(filtration_begin + bc1.death)) - sc_->filtration(*(filtration_begin + bc1.birth))
+            > sc_->filtration(*(filtration_begin + bc2.death)) - sc_->filtration(*(filtration_begin + bc2.birth)));
+    }
+    FilteredComplex * sc_;
+    std::size_t filtration_size_;
+  };
+
+  void output_diagram(std::ostream& ostream = std::cout) {
+    auto filtration_begin = cpx_->filtration_simplex_range().begin();
+    auto filtration_size = std::distance(filtration_begin, cpx_->filtration_simplex_range().end());
+
+    auto barcodes = matrix_.get_current_barcode();
+    cmp_intervals_by_length cmp(cpx_);
+    std::sort(std::begin(barcodes), std::end(barcodes), cmp);
+
+    for (const Barcode& barcode : barcodes) {
+      ostream << charac_ << "  " << barcode.dim << " "
+        << cpx_->filtration(*(filtration_begin + barcode.birth)) << " ";
+        if (barcode.death > filtration_size) {
+          ostream << "inf" << std::endl;
+        } else {
+          ostream << cpx_->filtration(*(filtration_begin + barcode.death)) << std::endl;
+        } 
+    }
+  }
+
+
+ private:
+  FilteredComplex* cpx_;
+  Matrix<Options> matrix_;
+  int charac_;
+};
+
+}  // namespace persistence_matrix
+}  // namespace Gudhi
+
+#endif  // PERSISTENCE_MATRIX_H
