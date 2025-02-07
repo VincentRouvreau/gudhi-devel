05c61e85677b9a4eda03f4fc47c24f1add19238e
Vincent Rouvreau <vincent.rouvreau@inria.fr>
2025-02-07 10:06:50 +0100

    New cubical_complex __init__ and __all__ management

--- src/python/gudhi/tensorflow/cubical_layer.py
+++ src/python/gudhi/tensorflow/cubical_layer.py
@@ -1,6 +1,6 @@
 import numpy               as np
 import tensorflow          as tf
-from ..cubical_complex  import CubicalComplex
+from .._cubical_complex  import CubicalComplex
 
 ######################
 # Cubical filtration #
@@ -20,7 +20,7 @@ def _Cubical(Xflat, Xdim, dimensions, homology_coeff_field):
 
     # Retrieve and output image indices/pixels corresponding to positive and negative simplices
     cof_pp = cc.cofaces_of_persistence_pairs()
-    
+
     L_cofs = []
     for dim in dimensions:
 
@@ -54,7 +54,7 @@ class CubicalLayer(tf.keras.layers.Layer):
 
     def call(self, X):
         """
-        Compute persistence diagram associated to a cubical complex filtered by some pixel values 
+        Compute persistence diagram associated to a cubical complex filtered by some pixel values
 
         Parameters:
             X (TensorFlow variable): pixel values of the cubical complex
@@ -62,7 +62,7 @@ class CubicalLayer(tf.keras.layers.Layer):
         Returns:
             List[Tuple[tf.Tensor,tf.Tensor]]: List of cubical persistence diagrams. The length of this list is the same than that of dimensions, i.e., there is one persistence diagram per homology dimension provided in the input list dimensions. Moreover, the finite and essential parts of the persistence diagrams are provided separately: each element of this list is a tuple of size two that contains the finite and essential parts of the corresponding persistence diagram, of shapes [num_finite_points, 2] and [num_essential_points, 1] respectively. Note that the essential part is always empty in cubical persistence diagrams, except in homology dimension zero, where the essential part always contains a single point, with abscissa equal to the smallest value in the complex, and infinite ordinate
         """
-        # Compute pixels associated to positive and negative simplices 
+        # Compute pixels associated to positive and negative simplices
         # Don't compute gradient for this operation
         Xflat = tf.reshape(X, [-1])
         Xdim, Xflat_numpy = X.shape, Xflat.numpy()
